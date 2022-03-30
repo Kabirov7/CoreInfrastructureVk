6f6926f17a7c1a7329e78d8a4053b859a0e59480
artur <arturkabirov16@gmail.com>
2022-03-30 20:46:13 +0300

    First commit

---
+++ src/test/java/com/example/coreinfrastructurevk/CoreInfrastructureVkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coreinfrastructurevk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreInfrastructureVkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
