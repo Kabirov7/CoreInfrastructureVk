2d8dceb6652746f44d95a92311fbc00ee320f7aa
artur <arturkabirov16@gmail.com>
2022-04-02 23:27:43 +0300

    Allow front to read users data

--- src/main/java/com/example/coreinfrastructurevk/security/WebSecurityConfig.java
+++ src/main/java/com/example/coreinfrastructurevk/security/WebSecurityConfig.java
@@ -67,7 +67,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable().csrf().disable()
+        http
+                .cors().and()
+                .csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/auth/**").permitAll()
